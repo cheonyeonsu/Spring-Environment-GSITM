019492c424a435ef3f1559076d99d17b2704d6e0
cheonyeonsu <cheonyeonsu@gmail.com>
2024-06-20 10:55:33 +0900

    댓글 삭제기능 구현 완료

--- src/main/java/com/mysite/sbb/answer/AnswerService.java
+++ src/main/java/com/mysite/sbb/answer/AnswerService.java
@@ -1,9 +1,11 @@
 package com.mysite.sbb.answer;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.mysite.sbb.DataNotFoundException;
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.user.SiteUser;
 
@@ -13,16 +15,35 @@ import lombok.RequiredArgsConstructor;
 @Service
 public class AnswerService {
 
-   private final AnswerRepository answerRepository;
-   
-   //									 content가 있어야 리파지토리에 담김
-   public void create(Question question, String content,SiteUser author) {
-      Answer answer = new Answer();
-      answer.setContent(content);
-      answer.setCreateDate(LocalDateTime.now());
-      answer.setQuestion(question); //질문에 대한 답변이므로, 해당 외래키 업데이트.
-      answer.setAuthor(author);
-      this.answerRepository.save(answer); //이 모든 내용을 리파지토리에 저장
-   }
-   
+	private final AnswerRepository answerRepository;
+
+	//									 content가 있어야 리파지토리에 담김
+	public void create(Question question, String content,SiteUser author) {
+		Answer answer = new Answer();
+		answer.setContent(content);
+		answer.setCreateDate(LocalDateTime.now());
+		answer.setQuestion(question); //질문에 대한 답변이므로, 해당 외래키 업데이트.
+		answer.setAuthor(author);
+		this.answerRepository.save(answer); //이 모든 내용을 리파지토리에 저장
+	}
+
+	//답변 조회
+	public Answer getAnswer(Integer id) {
+		Optional<Answer> answer = this.answerRepository.findById(id);
+		if (answer.isPresent()) {
+			return answer.get();
+		} else {
+			throw new DataNotFoundException("answer not found");
+		}
+	}
+
+	//답변 조회가 되어야 수정을 할 수 있음
+	public void modify(Answer answer, String content) {
+		answer.setContent(content);
+		answer.setModifyDate(LocalDateTime.now());
+		this.answerRepository.save(answer);
+	}
+
+
+
 }

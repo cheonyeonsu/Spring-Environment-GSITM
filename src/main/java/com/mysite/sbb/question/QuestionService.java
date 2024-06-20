7c704b51288fb11d9e65fb9cf715cb04d818b95f
cheonyeonsu <cheonyeonsu@gmail.com>
2024-06-20 10:33:20 +0900

    게시글 삭제기능 구현 완료

--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -49,5 +49,10 @@ public class QuestionService {
 	      question.setModifyDate(LocalDateTime.now());
 	      this.questionRepository.save(question);
 	   }
+   
+   //삭제하기
+   public void delete(Question question) {
+	   this.questionRepository.delete(question);
+   }
 
 }
